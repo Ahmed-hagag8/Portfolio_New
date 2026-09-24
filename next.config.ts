e0fc6f2480f073a31cfba2c8fe9cd43b75af2e46
Ahmed-hagag8 <a7med7agag13579@gmail.com>
2026-09-24 16:19:16 +0300

    ix: add basePath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? "/Portfolio_New" : "",
+  assetPrefix: isProd ? "/Portfolio_New/" : "",
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
